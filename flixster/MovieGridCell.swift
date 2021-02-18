efcb59258e49bc096096a3cb5f37369a4becfd47
Srilekha Nune <srilekhanune@gmail.com>
2021-02-18 12:33:53 -0800

    CollectionView added

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Srilekha Nune on 2/18/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
